eee2ca1638aeb2c5d77576a4a9e3ea30ad2e7d51
jiminji <sionyjm@gmail.com>
2024-12-23 15:04:18 +0900

    Back Final...mayb?

--- back/app.py
+++ back/app.py
@@ -7,7 +7,6 @@ from torchvision import models, transforms
 import base64
 import io
 
-# FastAPI 애플리케이션 생성
 app = FastAPI()
 index_class = {}
 try:
@@ -17,7 +16,7 @@ try:
     print("File Loaded")
 except Exception as e:
     print("class-index file loading failed")
-# CORS 설정 추가
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["https://resnet-web.vercel.app"],
@@ -26,7 +25,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# ResNet 모델 로드
 try:
     model = models.resnet50(pretrained=True)
     model.eval()
@@ -34,7 +32,6 @@ try:
 except Exception as e:
     print("Model loading failed")
 
-# 이미지 변환 파이프라인 정의
 try:
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -49,30 +46,26 @@ except Exception as e:
 async def predict(file: UploadFile = File(...)):
     try:
         print("File received:", file.filename)
-        # 파일이 이미지인지 확인
         try:
             image = Image.open(file.file).convert("RGB")
             print("Image opened")
         except UnidentifiedImageError:
             return JSONResponse(content={"error": "Uploaded file is not a valid image"}, status_code=400)
 
-        # 이미지 전처리
         try:
-            transformed_image = transform(image).unsqueeze(0)  # 배치 차원 추가
+            transformed_image = transform(image).unsqueeze(0)  
             print("Transformed")
         except Exception as e:
             print("Transform failed")
 
-        # 모델로 예측 수행
         try:
             with torch.no_grad():
                 outputs = model(transformed_image)
                 _, predicted = torch.max(outputs, 1)
-            print("Predict Success")
+            print(f"{int(predicted)} Predict Success")
         except Exception as e:
             print("Prediction error")
 
-        # 이미지를 base64로 인코딩
         try:
             buffered = io.BytesIO()
             image.save(buffered, format="JPEG")
@@ -80,7 +73,6 @@ async def predict(file: UploadFile = File(...)):
             print("Image saved")
         except Exception as e:
             print("Image uploading failed")
-        # 결과 반환
         return {
             "class": index_class[int(predicted.item())],
             "image": f"data:image/jpeg;base64,{image_base64}"
